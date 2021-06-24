7f5d08a9a8dd4cc4b97a415999aec288d713d7a3
Vitor Hardoim <vitorhardoim@gmail.com>
2021-06-24 16:56:11 -0300

    Possible bugfix for #6: single transaction on creation

--- src/controller/run.ts
+++ src/controller/run.ts
@@ -7,7 +7,7 @@ import BidwarOption from '../models/BidwarOption';
 import Game from '../models/Game';
 import Event from '../models/Event';
 
-import {EventRepo, RunRepo, RunRunnerRepo, SubmitRunRepo, RunIncentiveRepo, BidwarOptionRepo, GameRepo} from '../loaders/typeorm';
+import {EventRepo, RunRepo, RunRunnerRepo, SubmitRunRepo, RunIncentiveRepo, BidwarOptionRepo, GameRepo, connection} from '../loaders/typeorm';
 
 interface CtrlResponse{
   success?: any;
@@ -28,11 +28,16 @@ interface IOption{
 export default{
   async create(runnerId: string, gameId: string, category: string, estimatedTime: number, preferredTime: string, platform: string, incentives: IIncentive[]): Promise<CtrlResponse>{
     logger.log("info", "Starting create run function");
+    
+    const queryRunner = connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     try{
       //Find active event
       const activeEvent = await EventRepo.findOneOrFail({ active: 'A' });
 
-      //Create Run      
+      //Create Run
+      /*
       const run: Run = RunRepo.create({
         game_id: Number(gameId),
         category: category,
@@ -42,16 +47,38 @@ export default{
       });
 
       await RunRepo.save(run);
+      */
+      await queryRunner.manager.insert(Run, {
+        game_id: Number(gameId),
+        category: category,
+        estimated_time: estimatedTime,
+        preferred_time_slot: preferredTime,
+        platform: platform
+      });
+
+      let run = await RunRepo.findOne({
+        order: {
+          id: 'DESC'
+        }
+      });
+      if(!run) throw 'Run not found';
       
       //Create RunRunner
+      /*
       const runRunner: RunRunner = RunRunnerRepo.create({
         runner_id: Number(runnerId),
         run_id: run.id
       });
 
       await RunRunnerRepo.save(runRunner);
+      */
+      await queryRunner.manager.insert(RunRunner, {
+        runner_id: Number(runnerId),
+        run_id: run.id
+      });
       
       //Create SubmitRun      
+      /*
       const submitRun: SubmitRun = SubmitRunRepo.create({
         event_id: activeEvent.id,
         run_id: run.id,
@@ -61,6 +88,17 @@ export default{
       });
 
       await SubmitRunRepo.save(submitRun);
+      */
+      await queryRunner.manager.insert(SubmitRun, {
+        event_id: activeEvent.id,
+        run_id: run.id,
+        reviewed: false,
+        approved: false,
+        waiting: false
+      });
+
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
 
       //Create Incentives and Bidwar Options
       
@@ -95,6 +133,8 @@ export default{
 
       return { success: "Creation success" };
     }catch(error){
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
       logger.log("error", "DB Error: " + JSON.stringify(error));
       return {error: "Server error"};
     }
